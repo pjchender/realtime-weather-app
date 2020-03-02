e37e940001f1acab18a44e21e319761815ac9489
pjchender <pjchender@gmail.com>
2020-03-02 15:44:13 +0800

    fix: wrong sunset and sunrise time on Safari

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import styled from '@emotion/styled';
 import { ThemeProvider } from 'emotion-theming';
+import dayjs from 'dayjs';
 import WeatherCard from './WeatherCard';
 import useWeatherApi from './useWeatherApi';
 import sunriseAndSunsetData from './sunrise-sunset.json';
@@ -42,7 +43,7 @@ const getMoment = (locationName) => {
 
   if (!location) return null;
 
-  const now = new Date();
+  const now = dayjs();
   const nowDate = Intl.DateTimeFormat('zh-TW', {
     year: 'numeric',
     month: '2-digit',
@@ -53,13 +54,14 @@ const getMoment = (locationName) => {
 
   const locationDate =
     location.time && location.time.find((time) => time.dataTime === nowDate);
-  const sunriseTimestamp = new Date(
+  const sunriseTimestamp = dayjs(
     `${locationDate.dataTime} ${locationDate.sunrise}`
-  ).getTime();
-  const sunsetTimestamp = new Date(
+  ).unix();
+  const sunsetTimestamp = dayjs(
     `${locationDate.dataTime} ${locationDate.sunset}`
-  ).getTime();
-  const nowTimeStamp = now.getTime();
+  ).unix();
+
+  const nowTimeStamp = now.unix();
 
   return sunriseTimestamp <= nowTimeStamp && nowTimeStamp <= sunsetTimestamp
     ? 'day'
